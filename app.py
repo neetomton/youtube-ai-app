07ef7ab9f4b86a861b619ff07d76edd321444ae8
neetomton <31212733+neetomton@users.noreply.github.com>
2026-01-27 15:03:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ except:
 
 # 👇 本番公開後に、あなたのStreamlitアプリのURLに書き換えてください
 # （デプロイ直後は自動で割り当てられる https://youtube-ai-app-xxxx.streamlit.app のようなURLになります）
-YOUR_DOMAIN = "http://localhost:8501" 
+YOUR_DOMAIN = "youtube-ai-app-erc7asvwqhbytnos9miwsm" 
 
 # ==========================================
 # 🌐 多言語設定 / Language Settings
@@ -154,3 +154,4 @@ else:
                 st.link_button("👉 Stripe Checkout", checkout_session.url)
             except Exception as e:
                 st.error(f"Stripe Error: {e}")
+
